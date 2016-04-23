f86c1188fd1933a0f1264701e9b511b5ed3f9c14
Nada Alaa <nadaalaa.na@gmail.com>
2016-04-23 23:00:29 +0200

    Revert "Check Skybox Boundaries Added"
    This reverts commit 3c2560db7ca125109b72531ab6fd1f6410300f7d.

--- FirstPersonShooter/Renderer/Renderer.h
+++ FirstPersonShooter/Renderer/Renderer.h
@@ -2,52 +2,51 @@
 #define Renderer_h__
 
 #include <gl/glew.h>
-#include <glfw/glfw3.h>
+#include <gl/glfw3.h>
 #include <glm/glm.hpp>
 #include <glm/gtc/matrix_transform.hpp>
 #include <memory>
 
 
-#include "shader.hpp"
-#include "Model.h"
-#include "EulerCamera.h"
-#include "Cube.h"
-#include "texture.h"
-#include "ShaderProgram.h"
-#include "Model3D.h"
-#include "SkyBox.h"
+#include "Shaders/shader.hpp"
+#include "Model/Model.h"
+#include "EulerCamera/EulerCamera.h"
+#include "Cube/Cube.h"
+#include "Texture/texture.h"
+#include "ShaderProgram/ShaderProgram.h"
+#include "Scene/Model3D.h"
+#include"SkyBox/SkyBox.h"
 using namespace std;
 class Renderer
 {
-    
-    
+
+
     GLuint programID;
     ShaderProgram shader;
-    // Transformation
-    //GLuint MatrixID;
-    GLuint ModelMatrixID;
-    GLuint AmbientLightID;
-    GLuint LightPositionID;
-    GLuint EyePositionID;
-    
-    std::unique_ptr<SkyBox> Game_SkyBox;
-    unique_ptr<Model3D> soldier;
-    
-    std::unique_ptr<EulerCamera> myCamera;
-    
-    glm::vec3 ambientLight;
-    glm::vec3 lightPosition;
-    
+	// Transformation
+	//GLuint MatrixID;
+	GLuint ModelMatrixID;
+	GLuint AmbientLightID;
+	GLuint LightPositionID;
+	GLuint EyePositionID;
+
+	std::unique_ptr<SkyBox> Game_SkyBox;
+	unique_ptr<Model3D> soldier;
+
+	std::unique_ptr<EulerCamera> myCamera;
+
+	glm::vec3 ambientLight;
+	glm::vec3 lightPosition;
+
 public:
     Renderer();
     ~Renderer();
     
-    bool CheckCameraPosition(glm::vec3,GLuint,int);
     void Initialize();
     void Draw();
-    void HandleKeyboardInput(int key);
-    void HandleMouse(double deltaX,double deltaY);
-    void Update(double deltaTime);
+	void HandleKeyboardInput(int key);
+	void HandleMouse(double deltaX,double deltaY);
+	void Update(double deltaTime);
     void Cleanup();
 };
 
